50066186fb32a8c622f64d916b7e7f1026abdc98
simlloyd <business.simlloyd@gmail.com>
2026-06-14 17:14:31 +0800

    added error handling with try/catch

---
+++ controllers/userController.js
@@ -0,0 +1,47 @@
+const User = require('../models/User');
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong', error: error.message });
+    }
+    
+};
+
+const createUser = async (req, res) => {
+    try {
+        const newUser = new User(req.body);
+        await newUser.save();
+        res.json({ message: 'User saved!', user: newUser });
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong', error: error.message });
+    }
+     
+};
+
+const updateUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const user = await User.findByIdAndUpdate(id, updatedData, { returnDocument: 'after' });
+        res.json({ message: 'User updated!', user: user });
+    } catch (error) {
+        res.status(500).json({ message: 'Invalid ID or something went wrong', error: error.message });
+    }
+    
+};
+
+const deleteUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+        await User.findByIdAndDelete(id);
+        res.json({ message: 'User deleted!' });
+    } catch (error) {
+        res.status(500).json({ message: 'Invalid ID or something went wrong', error: error.message });
+    }
+    
+};
+
+module.exports = { getAllUsers, createUser, updateUser, deleteUser };
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const User = require('./User');
+const userRoutes = require('./routes/users');
 
 const app = express();
 app.use(express.json());
@@ -19,29 +19,7 @@ app.get('/', (req, res) => {
     res.send('Hello! My server is working!');
 });
 
-app.get('/users', async (req, res) => {
-    const users = await User.find();
-    res.json(users);
-});
-
-app.post('/users', async (req, res) => {
-    const newUser = new User(req.body);
-    await newUser.save();
-    res.json({ message: 'User saved!', user: newUser });
-});
-
-app.delete('/users/:id', async (req, res) => {
-    const id = req.params.id;
-    await User.findByIdAndDelete(id);
-    res.json({ message: 'User deleted!' });
-});
-
-app.put('/users/:id', async (req, res) => {
-    const id = req.params.id;
-    const updateData = req.body;
-    const user = await User.findByIdAndUpdate(id, updateData, { returnDocument: 'after' })
-    res.json({ message: 'User updated!', user: user });
-});
+app.use('/users', userRoutes);
 
 app.listen(3000, '0.0.0.0', () => {
     console.log('Server is running on port 3000');
